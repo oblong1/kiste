83937d2d1ed0e092b7c7bae8597210bac44a4e63
stock <redwine@onlinehome.de>
2020-03-31 02:23:18 +0200

    Ordner für Java Projekte angelegt

---
+++ java/krempel/Gruss.java
@@ -0,0 +1,12 @@
+public class Gruss {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Viel Erfolg beim");
+	System.out.println("Programmieren mit Java und Eclipse!!!!");
+
+    }
+
+}
